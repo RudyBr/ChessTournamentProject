eb5dc6905d1de276a4c51c443a68b47436e46e72
RudyBr <rudybroutin.pro@gmail.com>
2022-07-15 09:34:28 +0200

    La liste des joueurs prédéfinis est maintenant une liste d'instance de la classe Player

--- models/data_test.py
+++ models/data_test.py
@@ -1,10 +1,13 @@
+from player import Player
+
+
 PLAYERS_DETAILS = [
-            {"nom": "John", "prenom": "Doe", "date_naissance": "01-01-2000", "sexe": "M", "classement": int(1001)},
-            {"nom": "Jane", "prenom": "Smith", "date_naissance": "02-01-2000", "sexe": "F", "classement": int(1002)},
-            {"nom": "Martin", "prenom": "Luther", "date_naissance": "03-01-2000", "sexe": "M", "classement": int(1003)},
-            {"nom": "Jean", "prenom": "Bon", "date_naissance": "04-01-2000", "sexe": "M", "classement": int(1004)},
-            {"nom": "Paul", "prenom": "Hochon", "date_naissance": "05-01-2000", "sexe": "M", "classement": int(1005)},
-            {"nom": "Marie", "prenom": "Curry", "date_naissance": "06-01-2000", "sexe": "F", "classement": int(1006)},
-            {"nom": "Nicol", "prenom": "Bolas", "date_naissance": "07-01-2000", "sexe": "M", "classement": int(1007)},
-            {"nom": "Magnus", "prenom": "Carlsen", "date_naissance": "08-01-2000", "sexe": "M", "classement": int(1008)}
+            Player("John", "Doe", "01-01-2000", "M", 1001),
+            Player("Jane", "Smith", "02-01-2000", "F", 1002),
+            Player("Martin", "Luther", "03-01-2000", "M", 1003),
+            Player("Jean", "Bon", "04-01-2000", "M", 1004),
+            Player("Paul", "Hochon", "05-01-2000", "M", 1005),
+            Player("Marie", "Curry", "06-01-2000", "F", 1006),
+            Player("Nicol", "Bolas", "07-01-2000", "M", 1007),
+            Player("Magnus", "Carlsen", "08-01-2000", "M", 1008)
     ]
