71bb1e3f19a9c1148acda9b788fe6826f431de44
RudyBr <rudybroutin.pro@gmail.com>
2022-07-15 09:42:01 +0200

    inversion paramètres last_name first_name

--- models/player.py
+++ models/player.py
@@ -1,9 +1,9 @@
 class Player:
     PLAYER_NUMBER = 8
 
-    def __init__(self, last_name, first_name, birth_date, gender, ranking):
-        self.last_name = last_name
+    def __init__(self, first_name, last_name, birth_date, gender, ranking):
         self.first_name = first_name
+        self.last_name = last_name
         self.birth_date = birth_date
         self.gender = gender
         self.ranking = ranking
