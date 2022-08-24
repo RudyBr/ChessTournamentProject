f3924f41b9468aca21fb5e1d7ec6fb93add75df7
RudyBr <rudybroutin.pro@gmail.com>
2022-08-24 19:02:31 +0200

    exclusion __pycache__ sur gitignore, màj controller et modèle round

--- controllers/base.py
+++ controllers/base.py
@@ -15,33 +15,13 @@ class Controller:
         """Has a tournament and a view."""
         # models
         # self.tournament affecté dans get_tournament_details
-        # players
-        self.players = []
+        # tournanment
+        self.tournament = None
         # current_tournament
         self.current_tournament = None
         # views
         self.view = view
 
-    def get_players_details(self):
-        while len(self.tournament.player_list) < Player.PLAYER_NUMBER:
-            player_details = self.view.player_entry()
-            player = Player(player_details["nom"],
-                            player_details["prenom"],
-                            player_details["date_naissance"],
-                            player_details["sexe"],
-                            player_details["classement"])
-            self.players.append(player)
-
-    def get_tournament_details(self):
-        tournament_details = self.view.tournament_entry()
-        nombre_de_rondes = tournament_details["nombre de rondes"]
-        nombre_de_rondes = int(nombre_de_rondes) if nombre_de_rondes else 4
-        self.tournament = Tournament(tournament_details["nom"],
-                                     tournament_details["lieu"],
-                                     tournament_details["date"],
-                                     nombre_de_rondes,
-                                     tournament_details["controle de temps"],
-                                     tournament_details["description"])
 
     def player_module(self):
         while True:
@@ -61,7 +41,13 @@ class Controller:
             option = self.view.add_player_menu()
             if option == 1:  # Créer un nouveau joueur et l'ajouter au tournoi
                 data = self.view.new_player()
-                player = models.player.Player(data["first_name"], data["last_name"], data["birthday_date"], data["gender"], data["ranking"])
+                player = models.player.Player(
+                    data["first_name"],
+                    data["last_name"],
+                    data["birthday_date"],
+                    data["gender"],
+                    data["ranking"]
+                )
                 self.current_tournament.player_list.append(player)
             elif option == 2:  # Ajouter un joueur de la base de données au tournoi
                 pass
@@ -73,7 +59,14 @@ class Controller:
                 pass
             elif option == 2:  # Ajouter un nouveau  tournoi
                 data = self.view.new_tournament()
-                self.current_tournament = models.tournament.Tournament(data["name"], data["location"], data["date"], data["round_quantity"], data["time_control"], data["description"])
+                self.current_tournament = models.tournament.Tournament(
+                    data["name"],
+                    data["location"],
+                    data["date"],
+                    data["round_quantity"],
+                    data["time_control"],
+                    data["description"]
+                )
                 print(self.current_tournament)
                 # boucle : tant que nombre_de_joueur != 8 --> ajouter joueur
                 self.current_tournament.player_list = []
--- models/round.py
+++ models/round.py
@@ -19,5 +19,14 @@ class Round:
 
         self.ordered_player_list = sorted(self.tournament.player_list,
                                           key=lambda player: player.ranking)
+
+
+        # classement pour round 2+ avec comme 1er élément de tri le score et 2eme l'élo
+        self.ordered_player_list = sorted(self.tournament.player_list,
+                                          key=lambda player: (player.score, player.ranking))
+
+
         # il n'y a plus qu'à créer une liste de matchs avec toutes les paires
         # de joueurs de cette liste
+
+
