be8141e7c1e1538f4aeceb0f5e38361b89d8ca37
RudyBr <rudybroutin.pro@gmail.com>
2022-11-16 16:51:05 +0100

    serialisation de la classe Player

--- models/player.py
+++ models/player.py
@@ -2,6 +2,15 @@ class Player:
     PLAYER_NUMBER = 8
 
     def __init__(self, first_name, last_name, birth_date, gender, ranking):
+        """
+
+        :param first_name:
+        :param last_name:
+        :param birth_date:
+        :param gender:
+        :param ranking:
+        """
+        self.id = None
         self.first_name = first_name
         self.last_name = last_name
         self.birth_date = birth_date
@@ -16,9 +25,18 @@ class Player:
                f"Sexe: {self.gender} \n" \
                f"Classement Elo: {self.ranking}"
 
-
     def __repr__(self):
         return f"{self.first_name} {self.last_name} ranking: [{self.ranking}]  score: [{self.score}]"
 
     def civility(self):
         return f"{self.first_name} {self.last_name}"
+
+    def serialized_player(self):
+        return {"id": self.id if self.id is not None else 0,
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "birth_date": self.birth_date,
+                "gender": self.gender,
+                "ranking": self.ranking,
+                "score": self.score
+                }
