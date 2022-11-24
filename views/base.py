75d2ae2f4a8a590fa6a02ff6980aa51a8ee64005
RudyBr <rudybroutin.pro@gmail.com>
2022-11-24 10:27:30 +0100

    gestion erreurs entrées utilisateur menu choix multiple

--- views/base.py
+++ views/base.py
@@ -60,28 +60,46 @@ class View:
         | | | | (_| | | | | | | | | | | \__ \ |_| | | (_| | || (_) | |                          
         \_| |_/\__,_|_| |_| |_|_|_| |_|_|___/\__|_|  \__,_|\__\___/|_|                          
         """)
-        print("Choix 1  -->  Menu Joueurs")
-        print("Choix 2  -->  Menu Tournois")
-        print("Choix 3  -->  Menu des Rapports")
-        print("Choix 0  -->  Quitter le programme")
-        return int(input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   "))
+        while True:
+            print("Choix 1  -->  Menu Joueurs")
+            print("Choix 2  -->  Menu Tournois")
+            print("Choix 3  -->  Menu des Rapports")
+            print("Choix 0  -->  Quitter le programme")
+            choice = input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   ")
+            if not choice.isdigit() or int(choice) < 0 or int(choice) > 3:
+                print("Veuillez choisir une option valide.")
+                continue
+            else:
+                return int(choice)
 
 
     def player_menu(self):
-        print("\n")
-        print(r"\\\\\\\\\\\\\\\\  MENU JOUEURS  ////////////////")
-        print("Choix 1  -->  Liste des joueurs de la base de données")
-        print("Choix 2  -->  Ajouter un nouveau joueur à la base de données")
-        print("Choix 0  -->  Retour au menu principal")
-        return int(input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   "))
+        while True:
+            print("\n")
+            print(r"\\\\\\\\\\\\\\\\  MENU JOUEURS  ////////////////")
+            print("Choix 1  -->  Liste des joueurs de la base de données")
+            print("Choix 2  -->  Ajouter un nouveau joueur à la base de données")
+            print("Choix 0  -->  Retour au menu principal")
+            choice = input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   ")
+            if not choice.isdigit() or int(choice) < 0 or int(choice) > 2:
+                print("Veuillez choisir une option valide.")
+                continue
+            else:
+                return int(choice)
 
     def tournament_menu(self):
-        print("\n")
-        print(r"\\\\\\\\\\\\\\\\  MENU DU TOURNOI  ////////////////")
-        print("Choix 1  -->  Liste des joueurs participants au tournoi")
-        print("Choix 2  -->  Ajouter un nouveau tournoi")
-        print("Choix 0  -->  Retour au menu principal")
-        return int(input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   "))
+        while True:
+            print("\n")
+            print(r"\\\\\\\\\\\\\\\\  MENU DU TOURNOI  ////////////////")
+            print("Choix 1  -->  Liste des joueurs participants au tournoi")
+            print("Choix 2  -->  Ajouter un nouveau tournoi")
+            print("Choix 0  -->  Retour au menu principal")
+            choice = input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   ")
+            if not choice.isdigit() or int(choice) < 0 or int(choice) > 2:
+                print("Veuillez choisir une option valide.")
+                continue
+            else:
+                return int(choice)
 
     def new_tournament(self):
         print("\n")
@@ -96,7 +114,13 @@ class View:
                             "BLitz (10 minutes ou moins par joueur)\n"
                             "Coup rapide (de 10 à 60 minutes par joueur)  ")
         description = input("Description du tournoi   ")
-        return {"name":name, "location":location, "date":date, "round_quantity":round_quantity, "time_control":time_control, "description":description}
+        return {"name":name,
+                "location":location,
+                "date":date,
+                "round_quantity":round_quantity,
+                "time_control":time_control,
+                "description":description
+                }
 
     def new_player(self):
         print("\n")
@@ -110,26 +134,45 @@ class View:
                 "ranking": ranking}
 
     def add_player_menu(self):
-        print("\n")
-        print(r"\\\\\\\\\\\\\\\\  CHOIX DE L'AJOUT DE JOUEUR  ////////////////")
-        print("Choix 1  -->  Créer un nouveau joueur et l'ajouter au tournoi")
-        print("Choix 2  -->  Ajouter un joueur de la base de données au tournoi")
-        return int(input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   "))
+        while True:
+            print("\n")
+            print(r"\\\\\\\\\\\\\\\\  CHOIX DE L'AJOUT DE JOUEUR  ////////////////")
+            print("Choix 1  -->  Créer un nouveau joueur et l'ajouter au tournoi")
+            print("Choix 2  -->  Ajouter un joueur de la base de données au tournoi")
+            choice = input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   ")
+            if not choice.isdigit() or int(choice) < 1 or int(choice) > 2:
+                print("Veuillez choisir une option valide.")
+                continue
+            else:
+                return int(choice)
 
     def report_menu(self):
-        print("\n")
-        print(r"\\\\\\\\\\\\\\\\  MENU DES RAPPORTS  ////////////////")
-        print("Choix 1  -->  Affichage liste des joueurs")
-        print("Choix 2  -->  Affichage de tous les tournois")
-        print("Choix 0  -->  Retour au menu principal")
-        return int(input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   "))
+        while True:
+            print("\n")
+            print(r"\\\\\\\\\\\\\\\\  MENU DES RAPPORTS  ////////////////")
+            print("Choix 1  -->  Affichage liste des joueurs")
+            print("Choix 2  -->  Affichage de tous les tournois")
+            print("Choix 0  -->  Retour au menu principal")
+            choice = input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   ")
+            if not choice.isdigit() or int(choice) < 0 or int(choice) > 2:
+                print("Veuillez choisir une option valide.")
+                continue
+            else:
+                return int(choice)
 
     def player_listing_menu(self):
-        print("\n")
-        print(r"\\\\\\\\\\\\\\\\  AFFICHAGE DES JOUEURSDE LA BASE DE DONNEES  ////////////////")
-        print("Choix 1  -->  Lister les joueurs par ordre alphabétique")
-        print("Choix 2  -->  Lister les joueurs par classement elo")
-        print("Choix 0  -->  Retour au menu principal")
+        while True:
+            print("\n")
+            print(r"\\\\\\\\\\\\\\\\  AFFICHAGE DES JOUEURS DE LA BASE DE DONNEES  ////////////////")
+            print("Choix 1  -->  Lister les joueurs par ordre alphabétique")
+            print("Choix 2  -->  Lister les joueurs par classement elo")
+            print("Choix 0  -->  Retour au menu principal")
+            choice = input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   ")
+            if not choice.isdigit() or int(choice) < 0 or int(choice) > 2:
+                print("Veuillez choisir une option valide.")
+                continue
+            else:
+                return int(choice)
 
     def tournament_listing_menu(self):
         print("\n")
@@ -138,10 +181,16 @@ class View:
 
 
     def match_menu(self, joueur1_civility, joueur2_civility):
-        print("\n")
-        print(r"\\\\\\\\\\\\\\\\  RESULTAT DU MATCH  ////////////////")
-        print(f"Veuillez indiquer le résultat du match:  {joueur1_civility}  VS {joueur2_civility}")
-        print(f"Choix 1  -->  Le joueur {joueur1_civility} est vainqueur")
-        print(f"Choix 2  -->  Le joueur {joueur2_civility} est vainqueur")
-        print("Choix 3  -->  C'est un match nul")
-        return int(input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   "))
+        while True:
+            print("\n")
+            print(r"\\\\\\\\\\\\\\\\  RESULTAT DU MATCH  ////////////////")
+            print(f"Veuillez indiquer le résultat du match:  {joueur1_civility}  VS {joueur2_civility}")
+            print(f"Choix 1  -->  Le joueur {joueur1_civility} est vainqueur")
+            print(f"Choix 2  -->  Le joueur {joueur2_civility} est vainqueur")
+            print("Choix 3  -->  C'est un match nul")
+            choice = input("Tapez le numéro correspondant à votre choix, puis appuyez sur la touche Entrée.   ")
+            if not choice.isdigit() or int(choice) < 1 or int(choice) > 3:
+                print("Veuillez choisir une option valide.")
+                continue
+            else:
+                return int(choice)
