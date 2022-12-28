327ba3b65223e380bcc4e8b22d54deb190ae7f83
RudyBr <rudybroutin.pro@gmail.com>
2022-12-28 09:52:58 +0100

    serialisation classes Round et Match

--- models/match.py
+++ models/match.py
@@ -1,5 +1,16 @@
 class Match:
-    def __init__(self, joueur1, joueur2):
+    def __init__(self, _round, joueur1, joueur2):
+        self.id = None
+        self.round = _round
         self.joueur1 = joueur1
         self.joueur2 = joueur2
+        self.result = None
         # manque résultat
+
+    def serialize(self):
+        return {"id": self.id if self.id is not None else 0,
+                "round_id": self.round.id,
+                "joueur1": self.joueur1.id,
+                "joueur2": self.joueur2.id,
+                "result": self.result
+                }
--- models/round.py
+++ models/round.py
@@ -12,6 +12,7 @@ class Round:
         :param tournament:
         :param number: numéro de la ronde
         """
+        self.id = None
         self.tournament = tournament
         self.number = number
         self.match_list: List[Match] = []
@@ -34,7 +35,7 @@ class Round:
             list1 = ordered_player_list[:half_players_number]
             list2 = ordered_player_list[half_players_number:]
             for no_player in range(len(list1)):
-                match = Match(list1[no_player], list2[no_player])
+                match = Match(self, list1[no_player], list2[no_player])
                 self.match_list.append(match)
                 self.tournament.matches_history[match.joueur1].append(match.joueur2)
                 self.tournament.matches_history[match.joueur2].append(match.joueur1)
@@ -45,7 +46,7 @@ class Round:
                 while i < len(ordered_player_list):
                     if len(ordered_player_list) == 2 or \
                             ordered_player_list[i] not in self.tournament.matches_history[ordered_player_list[0]]:
-                        match = Match(ordered_player_list[0], ordered_player_list[i])
+                        match = Match(self, ordered_player_list[0], ordered_player_list[i])
                         print(ordered_player_list)
                         player_0 = ordered_player_list[0]
                         player_i = ordered_player_list[i]
@@ -65,5 +66,8 @@ class Round:
     def set_matches(self):
         ordered_player_list = self.tournament.get_ordered_player_list()
         self.players_pair(ordered_player_list)
-    # il n'y a plus qu'à créer une liste de matchs avec toutes les paires
-    # de joueurs de cette liste
+
+    def serialize(self):
+        return {"id": self.id if self.id is not None else 0,
+                "tournoi_id": self.tournament.id
+                }
